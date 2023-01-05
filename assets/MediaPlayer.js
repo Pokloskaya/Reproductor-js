afffc67baa9a31d146b62c43ef9e66ac58425907
Pokloskaya <master7n7@gmail.com>
2023-01-04 19:10:52 -0500

    Uploading base code

---
+++ assets/MediaPlayer.js
@@ -0,0 +1,24 @@
+class MediaPlayer {
+    constructor(config) {
+      this.media = config.el;
+      this.plugins = config.plugins || [];
+    }
+  
+    play() {
+      this.media.play();
+    }
+  
+    pause() {
+      this.media.pause();
+    }
+  
+    togglePlay() {
+      if (this.media.paused) {
+        this.play();
+      } else {
+        this.pause();
+      }
+    }
+  }
+  
+  export default MediaPlayer;
