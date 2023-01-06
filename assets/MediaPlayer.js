2b400184789999728a99a4fb7be0f79fadeaff6f
Pokloskaya <master7n7@gmail.com>
2023-01-06 18:59:13 -0500

    added new botton and exercices HTML

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -19,12 +19,28 @@ class MediaPlayer {
     pause() {
       this.media.pause();
     }
+
+    mute() {
+      this.media.muted = true;
+    }
+
+    unmute() {
+      this.media.muted = false;
+    }
   
     togglePlay() {
       if (this.media.paused) {
         this.play();
       } else {
         this.pause();
+      }
+    }
+
+    toggleMute() {
+      if (this.media.muted) {
+        this.unmute();
+      } else {
+        this.mute();
       }
     }
   }
--- assets/index.js
+++ assets/index.js
@@ -4,5 +4,10 @@ import AutoPlay from './plugins/AutoPlay.js';
 const video = document.querySelector('video');
 const player = new MediaPlayer({el:video, plugins: [new AutoPlay()]});
 
-const button = document.querySelector('button');
-button.onclick = () => player.togglePlay();
+//boton play/pause
+const button1 = document.querySelector('.button1');
+button1.onclick = () => player.togglePlay();
+
+//boton mute/unmute
+const button2 = document.querySelector('.button2');
+button2.onclick = () => player.toggleMute();
